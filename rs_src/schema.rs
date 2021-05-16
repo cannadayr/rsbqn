c23b85f3a39fe4c3bdc01478773d76d14f549c85
Rowan Cannaday <cannadayr@gmail.com>
2021-05-16 18:48:43 -0400

    add empty env to heap

--- rs_src/schema.rs
+++ rs_src/schema.rs
@@ -3,15 +3,30 @@ use std::sync::Mutex;
 
 pub type Id = u64;
 
+pub struct E {
+    parent: Id
+}
+impl E {
+    pub fn new(parent: Id) -> Self {
+        Self {
+            parent: parent
+        }
+    }
+}
+
 pub struct State {
     root: Id,
     id: Id,
+    heap: E,
 }
 impl State {
     pub fn new() -> Self {
+        let root = 0;
+        let e = E::new(root);
         Self {
-            root: 0,
-            id: 0
+            root: root,
+            id: 1+root,
+            heap: e,
         }
     }
     pub fn id(&self) -> Id {
