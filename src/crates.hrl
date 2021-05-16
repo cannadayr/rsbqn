daf09cfd85fe3f2b9fc3e4b8163e6f73e6d31bea
Rowan Cannaday <cannadayr@gmail.com>
2021-05-16 15:17:16 -0400

    rustler skeleton

---
+++ src/crates.hrl
@@ -0,0 +1,8 @@
+-ifndef(CRATES_HRL).
+-define(CRATES_HRL, 1).
+-define(crate_ebqn, undefined).
+-endif.
+-ifndef(FUNC_CRATES_HRL).
+-define(FUNC_CRATES_HRL, 1).
+-define(load_nif_from_crate(__APP,__CRATE,__INIT),(fun()->__PATH=filename:join(code:priv_dir(__APP),__CRATE),erlang:load_nif(__PATH,__INIT)end)()).
+-endif.
--- src/ebqn.erl
+++ src/ebqn.erl
@@ -1,3 +1,12 @@
 -module(ebqn).
 
--export([]).
+-include("crates.hrl").
+-on_load(init/0).
+
+-export([add/2]).
+
+init() ->
+    ?load_nif_from_crate(ebqn, ?crate_ebqn, 0).
+
+add(_X,_Y) ->
+    exit(nif_not_loaded).
