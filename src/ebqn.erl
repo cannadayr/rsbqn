4957445cd0eeeef6077f4971b114b787cab058bb
Rowan Cannaday <cannadayr@gmail.com>
2021-08-08 14:48:59 -0400

    use rustler-0.22. fix for latest rebar3_cargo.

--- src/ebqn.erl
+++ src/ebqn.erl
@@ -1,12 +1,12 @@
 -module(ebqn).
 
--include("crates.hrl").
+-include("cargo.hrl").
 -on_load(init/0).
 
 -export([init_st/0]).
 
 init() ->
-    ?load_nif_from_crate(ebqn, ?crate_ebqn, 0).
+    ?load_nif_from_crate(ebqn, 0).
 
 init_st() ->
     exit(nif_not_loaded).
