341cf0d96dd1480b8f4311a08a47604dafa4c986
Rowan Cannaday <cannadayr@gmail.com>
2021-08-15 22:05:30 -0400

    clean up duplicative code

--- rs_src/ebqn.rs
+++ rs_src/ebqn.rs
@@ -7,24 +7,16 @@ use std::sync::Arc;
 #[rustler::nif]
 fn init_st() -> NifResult<(Atom,ResourceArc<Container<'static>>)> {
     //[0,0,25],[5],[[0,1,0,0]]
-    let code0 = Code::default();
-    let bc = vec![0,0,25];
-    let objs = vec![V::Float(5.0)];
-    let code1 = Code {bc: bc, ..code0};
-    let code1arc = Arc::new(code1);
-    let block0 = Block::default();
-    let block1 = Block {typ: 0, imm: true, locals: 0, pos: 0, ..block0};
-    let block2 = Arc::new(block1);
-    block2.code.init(&code1arc);
-    let blocks = vec![block2];
-    code1arc.blocks.init(&blocks);
-
-    let env0: Env = Env::default();
-    let env1 = Env {vars: Vec::new(), ..env0};
-    let env1arc = Arc::new(env1);
+    let code  = Arc::new(Code {bc: vec![0,0,25], objs: vec![V::Float(5.0)], ..Code::default()});
+    let block = Arc::new(Block {typ: 0, imm: true, locals: 0, pos: 0, ..Block::default()});
+    block.code.init(&code);
+    let blocks = vec![block];
+    code.blocks.init(&blocks);
 
     let mut state = State::new();
-    state.alloc(env1arc);
+    let root_env = Arc::new(Env {vars: Vec::new(), ..Env::default()});
+    state.alloc(root_env);
+
     let mutex = Mutex::new(state);
     let container = Container { mutex };
 
