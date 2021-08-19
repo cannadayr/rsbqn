dc715ad43a4c577348e037266365f7d200beedf9
Rowan Cannaday <cannadayr@gmail.com>
2021-08-19 18:11:25 -0400

    add 2nd test case

--- rs_src/ebqn.rs
+++ rs_src/ebqn.rs
@@ -16,6 +16,9 @@ fn vm(state: &State,code: &Arc<Code>,block: &Arc<Block>,env: Cc<Mutex<Env>>,mut 
                 let r = code.objs[x].clone();
                 stack.push(r)
             },
+            14 => {
+                let _ = stack.pop();
+            },
             25 => {
                 break match stack.len() {
                     1 => {
@@ -38,7 +41,8 @@ fn vm(state: &State,code: &Arc<Code>,block: &Arc<Block>,env: Cc<Mutex<Env>>,mut 
 #[rustler::nif]
 fn init_st() -> NifResult<(Atom,ResourceArc<State>,V)> {
     //[0,0,25],[5],[[0,1,0,0]]
-    let code = Code::new(vec![0,0,25],&vec![V::Scalar(5.0)],vec![(0,true,0,0)]);
+    //let code = Code::new(vec![0,0,25],&vec![V::Scalar(5.0)],vec![(0,true,0,0)]); // 5
+    let code = Code::new(vec![0,0,14,0,1,25],&vec![V::Scalar(4.0),V::Scalar(3.0)],vec![(0,true,0,0)]); // 3
 
     let state = State::new();
 
