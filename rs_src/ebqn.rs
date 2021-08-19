56af322d1e901f08dd017acf2bc807469b0f9edc
Rowan Cannaday <cannadayr@gmail.com>
2021-08-18 22:20:35 -0400

    tweaking ref types, fitting stuff together

--- rs_src/ebqn.rs
+++ rs_src/ebqn.rs
@@ -6,22 +6,38 @@ use std::sync::Arc;
 use cc_mt::{Cc, Trace, Tracer, collect_cycles};
 use log::{debug, trace, error, log_enabled, info, Level};
 
-fn vm(state: &State,code: &Arc<Code>,block: &Arc<Block>,env: Cc<Mutex<Env>>) -> bool {
-    debug!("in VM\n");
+fn vm(state: &State,code: &Arc<Code>,block: &Arc<Block>,env: Cc<Mutex<Env>>,mut pos: usize,mut stack: Vec<Cc<V>>) -> bool {
+    debug!("block (typ,imm,locals,pos) : ({},{},{},{})",block.typ,block.imm,block.locals,block.pos);
+    let op = code.bc[pos];
+    debug!("pos : {}",pos);
+    loop {
+        let op = code.bc[pos];pos+=1;
+        match op {
+            0 => {
+                let x = code.bc[pos];pos+=1;
+                //let r = V::Cc(Cc::new(code.objs[x]));
+                let r = code.objs[x].clone();
+                stack.push(r.clone())
+            }
+            _ => {
+                error!("unreachable op");
+                panic!();
+            }
+        }
+        debug!("op : {}",op);
+    }
+
     true
 }
 
 #[rustler::nif]
 fn init_st() -> NifResult<(Atom,ResourceArc<State>)> {
     //[0,0,25],[5],[[0,1,0,0]]
-    let code = Code::new(vec![0,0,25],vec![V::Float(5.0)],vec![(0,true,0,0)]);
+    let code = Code::new(vec![0,0,25],&vec![V::Float(5.0)],vec![(0,true,0,0)]);
 
     let state = State::new();
 
-    let rtn = vm(&state,&code,&code.blocks[0],state.root.clone());
-
-    //let mutex = Mutex::new(state);
-    //let container = Container { mutex };
+    let rtn = vm(&state,&code,&code.blocks[0],state.root.clone(),code.blocks[0].pos,Vec::new());
 
     Ok((ok(),ResourceArc::new(state)))
 }
--- rs_src/schema.rs
+++ rs_src/schema.rs
@@ -2,24 +2,33 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use once_cell::sync::OnceCell;
 use cc_mt::{Cc, Trace, Tracer, collect_cycles};
+use log::{debug, trace, error, log_enabled, info, Level};
 
 rustler::atoms!{ok}
 
-#[derive(Debug)]
+#[derive(Debug,Clone,Copy)]
 pub enum V {
     Float(f64),
     BlockInst,
+    //Cc(Cc<V>),
+}
+impl Trace for V {
+    fn trace(&self, tracer: &mut Tracer) {
+        debug!("clearing V");
+    }
 }
 pub type Vn = Option<V>;
+//type Stack = Vec<V>;
 #[derive(Default,Debug)]
 pub struct Code<'a> {
     pub bc:    Vec<usize>,
-    pub objs:  Vec<V>,
+    pub objs:  Vec<Cc<V>>,
     pub blocks:LateInit<Vec<Arc<Block<'a>>>>,
 }
 impl<'a> Code<'a> {
-    pub fn new(bc: Vec<usize>,objs: Vec<V>,blocks_raw: Vec<(u8,bool,usize,usize)>) -> Arc<Self> {
-        let code = Arc::new(Self {bc: bc, objs: objs, ..Code::default()});
+    pub fn new(bc: Vec<usize>,objs_raw: &Vec<V>,blocks_raw: Vec<(u8,bool,usize,usize)>) -> Arc<Self> {
+        let objs_derv = objs_raw.iter().map(|obj| Cc::new(*obj)).collect::<Vec<Cc<V>>>();
+        let code = Arc::new(Self {bc: bc, objs: objs_derv, ..Code::default()});
         let blocks_derv = blocks_raw.iter().map(|block|
             match block {
                 (typ,imm,locals,pos) => {
@@ -40,14 +49,13 @@ pub struct Block<'a> {
 }
 #[derive(Default,Debug)]
 pub struct Env<'a> {
-    pub parent:LateInit<&'a Arc<Env<'a>>>,
+    pub parent:LateInit<&'a Cc<Env<'static>>>,
     pub vars:   Vec<Vn>,
 }
 impl<'a> Trace for Env<'a> {
     fn trace(&self, tracer: &mut Tracer) {
-        println!("tracing!");
+        debug!("clearing env");
     }
-
 }
 
 struct BlockInst<'a> {
@@ -64,17 +72,7 @@ impl State {
     pub fn new() -> Self {
         Self {root: Cc::new(Mutex::new(Env{vars: Vec::new(), ..Env::default()}))}
     }
-    /*
-    pub fn alloc(&mut self,env: Cc<Mutex<Env>>) {
-        self.heap.push(env);
-    }
-    */
 }
-/*
-pub struct Container<'a> {
-    pub mutex: Mutex<State<'a>>,
-}
-*/
 
 // https://docs.rs/once_cell/1.8.0/once_cell/#lateinit
 // https://github.com/rust-lang/rfcs/pull/2788
