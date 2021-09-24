cf927ec298125e319392cc164f079fb3b1d43f4f
Rowan Cannaday <cannadayr@gmail.com>
2021-09-24 14:25:45 -0400

    replace env_logger w/ syslog & log_panic
    this is due to output as documented in following github issue:
    https://github.com/rusterlium/rustler/issues/335
    
    currently uses USER syslog facility

--- rs_src/lib.rs
+++ rs_src/lib.rs
@@ -1,10 +1,15 @@
 mod schema;
 mod ebqn;
 use rustler::{Env,Term};
+use syslog::{Facility, Error};
+extern crate log_panics;
 
 pub fn load(env: Env, _info: Term) -> bool {
     rustler::resource!(schema::Env, env);
-    env_logger::init();
+    syslog::init(Facility::LOG_USER,
+                 log::LevelFilter::Debug,
+                 Some("ebqn"));
+    log_panics::init();
     true
 }
 rustler::init!("ebqn", [ebqn::init_st,ebqn::tests],load=load);
