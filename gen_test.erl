b8ed9a8387f5944a20a5cbaff9a99d0cf1aa6694
Rowan Cannaday <cannadayr@gmail.com>
2021-10-17 11:25:41 -0400

    start to handle more test formats
    prim tests not yet generating

--- gen_test.erl
+++ gen_test.erl
@@ -28,15 +28,19 @@ cmd_receive(Port, Acc) ->
         {Port, eof}          -> {ok, lists:reverse(Acc)}
     end.
 
+gen_line(assert,Code,undefined) ->
+    [<<"\tpanic::catch_unwind(|| { run(">>,Code,<<"); });\n">>];
+gen_line(assert,Code,Comment) ->
+    [<<"\tdebug!(\"test: ">>,unicode:characters_to_binary(erlang:binary_to_list(Comment)),<<"\");">>,<<"panic::catch_unwind(|| { run(">>,Code,<<"); }); // ">>,unicode:characters_to_binary(erlang:binary_to_list(Comment)),<<"\n">>];
+gen_line(Expected,Code,undefined) ->
+    [<<"\tassert_eq!(">>,erlang:float_to_binary(Expected,[{decimals, 1}]),<<",run(">>,Code,<<"));\n">>];
+gen_line(Expected,Code,Comment) ->
+    [<<"\tdebug!(\"test: ">>,unicode:characters_to_binary(erlang:binary_to_list(Comment)),<<"\");">>,<<"assert_eq!(">>,erlang:float_to_binary(Expected,[{decimals, 1}]),<<",run(">>,Code,<<")); // ">>,unicode:characters_to_binary(erlang:binary_to_list(Comment)),<<"\n">>].
 gen_code([],Accm) ->
     lists:reverse(Accm);
 gen_code(Todo,Accm) ->
     {Expected,Code,Comment} = hd(Todo),
-    Line =
-        case Comment of
-            undefined -> [<<"\tassert_eq!(">>,Expected,<<",run(">>,Code,<<"));\n">>];
-            C         -> [<<"\tdebug!(\"test: ">>,unicode:characters_to_binary(erlang:binary_to_list(C)),<<"\");">>,<<"assert_eq!(">>,Expected,<<",run(">>,Code,<<")); // ">>,unicode:characters_to_binary(erlang:binary_to_list(C)),<<"\n">>]
-        end,
+    Line = gen_line(Expected,Code,Comment),
     gen_code(tl(Todo),[Line] ++ Accm).
 gen_tests(_Repo,[],Accm) ->
     lists:reverse(Accm);
@@ -45,25 +49,42 @@ gen_tests(Repo,Args,Accm) ->
     {ok,CurDir} = file:get_cwd(),
     {Cmd,CmdArgs} = { erlang:binary_to_list(filename:join([CurDir, <<"crs.bqn">>])),[Repo,io_lib:format("~ts",[Code])] },
     {ok,Result} = cmd(Cmd,CmdArgs),
-    gen_tests(Repo,tl(Args),[{erlang:float_to_binary(Expected,[{decimals, 1}]),string:trim(Result),Comment}]++Accm).
+    gen_tests(Repo,tl(Args),[{Expected,string:trim(Result),Comment}]++Accm).
 parse_code([Code]) ->
     {Code,undefined};
 parse_code([Code,Comment]) ->
     {Code,Comment}.
-parse_line(Line,Accm)
+has_pct(Line) ->
+    case binary:match(Line,<<"%">>) of
+        nomatch -> false;
+        _ -> true
+    end.
+% # comment
+parse_line(Line,Accm,_HasPct)
     when binary_part(Line,0,1) =:= <<"#">>;
          Line =:= <<>> ->
     Accm;
-parse_line(Line,Accm) ->
+%% ! % expression
+parse_line(Line,Accm,_HasPct)
+    when binary_part(Line,0,1) =:= <<"!">> ->
+    [_Head,Tail] = binary:split(Line, <<"%">>, [global]),
+    {Code,Comment} = parse_code(binary:split(Tail, <<"#">>, [global])),
+    [{assert,Code,Comment}] ++ Accm;
+% result % expression
+parse_line(Line,Accm,true) ->
     [Head,Tail] = binary:split(Line, <<"%">>, [global]),
     Expected = float(erlang:list_to_integer(string:trim(erlang:binary_to_list(Head)))),
     {Code,Comment} = parse_code(binary:split(Tail, <<"#">>, [global])),
-    [{Expected,Code,Comment}] ++ Accm.
+    [{Expected,Code,Comment}] ++ Accm;
+% expression
+parse_line(Line,Accm,false) ->
+    {Code,Comment} = parse_code(binary:split(Line, <<"#">>, [global])),
+    [{1.0,Code,Comment}] ++ Accm.
 parse([],Accm) ->
     lists:reverse(Accm);
 parse(Lines,Accm) ->
     Line = hd(Lines),
-    parse(tl(Lines),parse_line(Line,Accm)).
+    parse(tl(Lines),parse_line(Line,Accm,has_pct(Line))).
 suite(Repo,Name) ->
     Path = filename:join([Repo,<<"test/cases/">>,Name]),
     {ok, Data} = file:read_file(Path),
@@ -72,11 +93,13 @@ suite(Repo,Name) ->
     gen_code(Tests,[]).
 main([Repo]) ->
     ByteCode = suite(Repo,<<"bytecode.bqn">>),
+    PrimCode = suite(Repo,<<"prim.bqn">>),
     io:format("~ts~n",[erlang:iolist_to_binary([
         <<"use log::{debug};\n">>,
         <<"use crate::ebqn::run;\n">>,
         <<"use crate::schema::{Code,new_scalar,Body};\n">>,
-        <<"pub fn bytecode() {\n">>,ByteCode,<<"\n}">>
+        <<"pub fn bytecode() {\n">>,ByteCode,<<"\n}">>,
+        <<"pub fn prim() {\n">>,PrimCode,<<"\n}">>
     ])]);
 main(_Args) ->
     io:format("bad arguments~n"),
