d5388cd3a1aac6620e8be98900a04d821f0f9f5e
Rowan Cannaday <cannadayr@gmail.com>
2021-12-05 18:48:04 -0500

    simplify monadic FN2O

--- rs_src/ebqn.rs
+++ rs_src/ebqn.rs
@@ -146,15 +146,11 @@ pub fn vm(env: &Env,code: &Cc<Code>,mut pos: usize,mut stack: Vec<Vs>) -> Vs {
                 let w = stack.pop().unwrap();
                 let f = stack.pop().unwrap();
                 let x = stack.pop().unwrap();
-                let arity =
-                    match &w.to_ref() {
-                        V::Nothing => 1,
-                        _ => 2,
-                    };
                 let r =
-                    match &x.to_ref() {
-                        V::Nothing => x,
-                        _ => call(arity,Some(f.to_ref().clone()),Some(x.to_ref().clone()),Some(w.to_ref().clone())),
+                    match (&x.to_ref(),&w.to_ref()) {
+                        (V::Nothing,_) => x,
+                        (_,V::Nothing) => call(1,Some(f.to_ref().clone()),Some(x.to_ref().clone()),None),
+                        _ => call(2,Some(f.to_ref().clone()),Some(x.to_ref().clone()),Some(w.to_ref().clone()))
                     };
                 stack.push(r);
                 dbg_stack_out("FN2C",pos-1,&stack);
