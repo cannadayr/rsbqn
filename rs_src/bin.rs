d70fb21d983bd73ddbaaf31e9d65c2d7da555ac1
Rowan Cannaday <cannadayr@gmail.com>
2022-01-17 16:40:27 -0500

    init an array of Vs::Nothing before execution

--- rs_src/bin.rs
+++ rs_src/bin.rs
@@ -3,7 +3,7 @@ use core::f64::{INFINITY,NEG_INFINITY};
 use ebqn::init_log;
 use ebqn::ebqn::{run,call,runtime,prog};
 use ebqn::code::{r0,r1,c};
-use ebqn::schema::{new_string,new_char,V};
+use ebqn::schema::{new_string,new_char,V,Vs};
 use ebqn::prim::{provide,decompose,prim_ind};
 use ebqn::schema::Body;
 use ebqn::schema::new_scalar;
@@ -19,6 +19,10 @@ fn main() {
     init_log();
     #[cfg(feature = "dhat")]
     let _dhat = Dhat::start_heap_profiling();
+
+    const SIZE: usize = 128;
+    const INIT: Vs = Vs::Nothing;
+    let stack: [Vs; SIZE] = [INIT; SIZE];
 
     // each function one-at-a-time
     let runtime = runtime();
